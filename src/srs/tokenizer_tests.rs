cc14429e3603fb1aa5f18f43b7c3bb278757554e
Christian Schwarz <me@cschwarz.com>
2017-02-18 11:48:26 +0100

    More tests for tokenizer with abstracted assertion routine.

--- src/srs/tokenizer_tests.rs
+++ src/srs/tokenizer_tests.rs
@@ -2,30 +2,44 @@
 mod test {
 
     use srs::tokenizer::{Tokenizer,Token};
+    use srs::tokenizer::Token::*;
+
+    fn assert_tokenize(input: &str, expected: Vec<Token> ) {
+        let t = Tokenizer::tokenize(input);
+        let result: Vec<Token> = t.collect();
+
+        if result != expected {
+            println!{"Not equal:\n\tE: {:?}\n\tR: {:?}", expected, result};
+        }
+        assert!(result == expected);
+    }
 
     #[test]
-    fn it_disassembles_a_string() {
-        let t = Tokenizer::tokenize("SOME=foo==lala@user@example.com");
+    fn it_tokenizes_complex_examples() {
+        assert_tokenize("SOME=foo==lala@user@example.com",
+                        vec![
+                        Text("SOME"),
+                        SRSSeparator,
+                        Text("foo"),
+                        SRSSeparator,
+                        SRSSeparator,
+                        Text("lala"),
+                        LocalDomainSeparator,
+                        Text("user"),
+                        LocalDomainSeparator,
+                        Text("example.com")
+                        ]);
+    }
 
-        use srs::tokenizer::Token::*;
+    #[test]
+    fn it_tokenizes_empty_string() {
+        assert_tokenize("", vec![]);
+    }
 
-        let expected = vec![
-            Text("SOME"),
-            SRSSeparator,
-            Text("foo"),
-            SRSSeparator,
-            SRSSeparator,
-            Text("lala"),
-            LocalDomainSeparator,
-            Text("user"),
-            LocalDomainSeparator,
-            Text("example.com")
-        ];
-        let c: Vec<Token> = t.collect();
-        println!("");
-        println!("{:?}", expected);
-        println!("{:?}", c);
-        assert!(c == expected);
-
+    #[test]
+    fn it_tokenizes_without_local_domain_separator() {
+        assert_tokenize("user==foo", vec![
+            Text("user"),SRSSeparator,SRSSeparator,Text("foo"),
+        ]);
     }
 }
